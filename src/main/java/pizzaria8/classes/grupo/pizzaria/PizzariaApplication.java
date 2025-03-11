9adbbcb2b8d78b45aa06089305a56c629f7121ab
Carolina Eskenazi <carolinae3@al.insper.edu.br>
2025-03-11 16:24:15 -0300

    dvd

---
+++ src/main/java/pizzaria8/classes/grupo/pizzaria/PizzariaApplication.java
@@ -0,0 +1,13 @@
+package pizzaria8.classes.grupo.pizzaria;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PizzariaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PizzariaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pizzaria8/classes/grupo/pizzaria/PizzariaApplicationTests.java
@@ -0,0 +1,13 @@
+package pizzaria8.classes.grupo.pizzaria;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PizzariaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
